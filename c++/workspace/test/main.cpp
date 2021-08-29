cfad04b5dd12ca5ccde0684c7be533101c5b3bab
wangk71 <wangk71@chinaunicom.cn>
2021-08-29 22:51:24 +0800

    update

--- c++/workspace/test/main.cpp
+++ c++/workspace/test/main.cpp
@@ -1,8 +1,9 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
-class Calc{
+class test{
 
 public:
     void add(int &a){
@@ -11,13 +12,33 @@ public:
         cout<<("add中的a值："+to_string(a))<<endl;
         //return a;
     }
+    void vectors(){
+        vector<int> vec ={1,2,3};
+        vector<int>::iterator iter;
+        cout<<"vec的值为：";
+        for (iter = vec.begin(); iter != vec.end() ; iter++) {
+            cout<<*iter<<"   ";
+        }
+
+        //assign
+        vector<int> v1;
+        v1.assign(vec.begin(),vec.end());
+        cout<<endl<<"vector 中copy的元素：";
+        for(iter = v1.begin() ; iter != v1.end() ; ++iter)
+        {
+            cout<<*iter<<" ";
+        }
+
+    }
+
 };
 
 int main() {
-    Calc c ;
-    int a = 100;
-    c.add(a);
-    cout<<("处理后的a值："+to_string(a))<<endl;
-
+    // 取地址符
+    test c ;
+//    int a = 100;
+//    c.add(a);
+//    cout<<("处理后的a值："+to_string(a))<<endl;
+    c.vectors();
     return 0;
 }
