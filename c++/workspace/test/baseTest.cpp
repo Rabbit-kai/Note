39edabe1fa1c7747878f1a677d12e4425e26000d
账户/账务中心-王凯 <wangk71@chinaunicom.cn>
2021-09-01 09:40:31 +0800

    update

--- c++/workspace/test/baseTest.cpp
+++ c++/workspace/test/baseTest.cpp
@@ -5,16 +5,16 @@
 #include "baseTest.h"
 
 
-void add(int &a){
+void baseTest::add(int &a){
     cout<<&a<<endl;
     a = a+1;
     cout<<("a in add is "+to_string(a))<<endl;
     //return a;
 }
-void vectors(){
+void baseTest::vectors(){
     vector<int> vec ={1,2,3};
     vector<int>::iterator iter;
-    cout<<"vec is ";
+    cout<<"vec is :";
     for (iter = vec.begin(); iter != vec.end() ; iter++) {
         cout<<*iter<<"   ";
     }
@@ -22,7 +22,7 @@ void vectors(){
     //assign
     vector<int> v1;
     v1.assign(vec.begin(),vec.end());
-    cout<<endl<<"vector 中copy的元素：";
+    cout<<endl<<"vector : ";
     for(iter = v1.begin() ; iter != v1.end() ; ++iter)
     {
         cout<<*iter<<" ";
